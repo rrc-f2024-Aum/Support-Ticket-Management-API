9ef20d56828b4904619afd274869eeb081ec8b5a
rrc_f2024_aum <amistry@academic.rrc.ca>
2026-02-05 10:51:17 -0600

    feat: added input validation for createNewTicket function.

--- src/api/v1/services/ticketService.ts
+++ src/api/v1/services/ticketService.ts
@@ -9,11 +9,23 @@ export const getAllTickets = (): Tickets[] => {
 export const createNewTicket = async (ticketData: {
     title: string,
     description: string,
-    priority: "low" | "medium" | "high" | "critical"
-    }): Promise<Tickets> => {
-    
+    priority: "low" |"medium" | "high" | "critical"
+}): Promise<Tickets> => {
+
+    if (!ticketData.title) {
+        throw new Error("Missing required field: title")
+    }
+
+    if (!ticketData.description) {
+        throw new Error("Missing required field: description")
+    }
+
+    if (!ticketData.priority) {
+        throw new Error("Invalid priority. Must be one of: critical, high, medium, low")
+    }
+
     const newTicket: Tickets = {
-        id: sampleTickets.length > 0? Math.max(...sampleTickets.map(t =>t.id)) + 1:1,
+        id: sampleTickets.length > 0 ? Math.max(...sampleTickets.map(t => t.id)) + 1 : 1,
         title: ticketData.title,
         description: ticketData.description,
         priority: ticketData.priority,
