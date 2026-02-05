b6c4ddb5c97f974b4868987b174c9f0aedb17dbc
rrc_f2024_aum <amistry@academic.rrc.ca>
2026-02-05 10:34:48 -0600

    feat: added function that creates new tickets with 3 required fields.

--- src/api/v1/services/ticketService.ts
+++ src/api/v1/services/ticketService.ts
@@ -5,7 +5,26 @@ export const getAllTickets = (): Tickets[] => {
     return sampleTickets;
 }
 
-// create ticket
+// create ticket - title, description and priority
+export const createNewTicket = async (ticketData: {
+    title: string,
+    description: string,
+    priority: "low" | "medium" | "high" | "critical"
+    }): Promise<Tickets> => {
+    
+    const newTicket: Tickets = {
+        id: sampleTickets.length > 0? Math.max(...sampleTickets.map(t =>t.id)) + 1:1,
+        title: ticketData.title,
+        description: ticketData.description,
+        priority: ticketData.priority,
+        status: "open",
+        createdAt: new Date().toISOString()
+    };
+
+    sampleTickets.push(newTicket);
+    return newTicket;
+}
+
 // get ticket by ID 
 // update ticket  
 // delete ticket
