f9a924b90efe0a57f292c5e4154e7a1030b013a7
rrc_f2024_aum <amistry@academic.rrc.ca>
2026-02-05 09:56:14 -0600

    feat: created new dir and service file, also added getAllTickets service function.

---
+++ src/api/v1/services/ticketService.ts
@@ -0,0 +1,11 @@
+import { Tickets, sampleTickets } from "src/data/ticketData";
+
+// Returns all the tickets.
+export const getAllTickets = (): Tickets[] => {
+    return sampleTickets;
+}
+
+// create ticket
+// get ticket by ID 
+// update ticket  
+// delete ticket
