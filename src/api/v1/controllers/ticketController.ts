997284dc612f48bb5f7f820f65a015cd401f3856
rrc_f2024_aum <amistry@academic.rrc.ca>
2026-02-05 14:32:59 -0600

    feat: added generateTicket controller function.

--- src/api/v1/controllers/ticketController.ts
+++ src/api/v1/controllers/ticketController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { HTTP_STATUS } from "src/constants/httpConstants";
 import * as ticketService from "../services/ticketService";
 
+// all tickets
 export const displayAllTickets = async (req: Request, res: Response): Promise<void> => {
     const tickets = await ticketService.getAllTickets();
     res.status(HTTP_STATUS.OK).send({
@@ -9,3 +10,19 @@ export const displayAllTickets = async (req: Request, res: Response): Promise<vo
         data: tickets
     });
 }
+
+// create ticket
+export const generateTicket = async (req: Request, res: Response): Promise<void> => {
+    const { title, description, priority } = req.body;
+    const addTicket = await ticketService.createNewTicket({
+        title, description, priority
+    });
+    res.status(HTTP_STATUS.CREATED).json({message: "Ticket created successfully", 
+        data: addTicket
+    });
+}
+
+// ticket only by id
+// update ticket details
+// delete ticket
+// calculate urgency 
