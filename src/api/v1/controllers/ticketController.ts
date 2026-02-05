bd99c3fa48688cf3ac3d1ba9e18d4d0bc9318a39
rrc_f2024_aum <amistry@academic.rrc.ca>
2026-02-05 14:12:10 -0600

    feat: added displayAllTickets controller function.

--- src/api/v1/controllers/ticketController.ts
+++ src/api/v1/controllers/ticketController.ts
@@ -2,3 +2,10 @@ import { Request, Response } from "express";
 import { HTTP_STATUS } from "src/constants/httpConstants";
 import * as ticketService from "../services/ticketService";
 
+export const displayAllTickets = async (req: Request, res: Response): Promise<void> => {
+    const tickets = await ticketService.getAllTickets();
+    res.status(HTTP_STATUS.OK).send({
+        message: "Tickets Received.",
+        data: tickets
+    });
+}
