6d25036f5c467f530f0cb4244518948fdb89c9f3
rrc_f2024_aum <amistry@academic.rrc.ca>
2026-02-06 08:17:03 -0600

    feat: added updateTicketDetails controller function.

--- src/api/v1/controllers/ticketController.ts
+++ src/api/v1/controllers/ticketController.ts
@@ -24,7 +24,7 @@ export const generateTicket = async (req: Request, res: Response): Promise<void>
 
 // ticket only by id
 export const displayTicketById = async (req: Request, res: Response): Promise<void> => {
-    const idParam = Number(req.params.id)
+    const idParam = Number(req.params.id);
     const ticket = await ticketService.getTicketById(idParam);
 
     if (!ticket) {
@@ -33,11 +33,42 @@ export const displayTicketById = async (req: Request, res: Response): Promise<vo
         });
         return;
     }
-    
+
     res.status(HTTP_STATUS.OK).json({ message: "Ticket received successfully",
         data: ticket
     })
 }
+
 // update ticket details
+export const updateTicketDetails = async (req: Request, res: Response): Promise<void> =>{
+    const idParam = Number(req.params.id);
+    const {title, description, priority, status} = req.body;
+
+    if (!idParam) {
+        res.status(HTTP_STATUS.BAD_REQUEST).json({
+            message: "Missing required field: id"
+        });
+        return;
+    }
+    const updatedTicket = await ticketService.updateTicket(idParam, {
+        title,
+        description,
+        priority,
+        status
+    });
+
+    if (!updatedTicket) {
+        res.status(HTTP_STATUS.NOT_FOUND).json({
+            message: "Ticket not found"
+        });
+        return;
+    }
+    res.status(HTTP_STATUS.OK).json({
+        message: "Ticket updated successfully",
+        data: updatedTicket
+    });
+
+}
+
 // delete ticket
 // calculate urgency 
