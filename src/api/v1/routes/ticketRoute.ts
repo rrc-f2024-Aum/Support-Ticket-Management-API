ea7fd3278bb9693c244475564db37752274f2049
rrc_f2024_aum <amistry@academic.rrc.ca>
2026-02-07 09:59:47 -0600

    fix: modified import statement as it was importing functions from service instead of controller file.

--- src/api/v1/routes/ticketRoute.ts
+++ src/api/v1/routes/ticketRoute.ts
@@ -1,20 +1,20 @@
 import express, { Router } from "express";
 import {
-    getAllTickets,
-    getTicketById,
-    createNewTicket,
-    //updateTicket,
-    deleteTicket,
-    calculateTicketUrgency
-} from "../services/ticketService";
+    displayAllTickets,
+    displayTicketById,
+    generateTicket,
+    updateTicketDetails,
+    removeTicket,
+    ticketUrgency
+} from "../controllers/ticketController";
 
 const router: Router = express.Router();
 
-router.get('/tickets', getAllTickets);
-router.get('/tickets/ :id', getTicketById);
-router.post('/tickets', createNewTicket);
-//router.put('/tickets/ :id', updateTicket);
-router.delete('/tickets/ :id', deleteTicket);
-router.get('/tickets/ :id/urgency', calculateTicketUrgency); 
+router.get('/tickets', displayAllTickets);
+router.get('/tickets/:id', displayTicketById);
+router.post('/tickets', generateTicket);
+router.put('/tickets/:id', updateTicketDetails);
+router.delete('/tickets/:id', removeTicket);
+router.get('/tickets/:id/urgency', ticketUrgency); 
 
 export default router;
